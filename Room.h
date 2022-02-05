8d9cbc3e5150ebbcfa54c5382ecaaf5e6c486e1c
rychkin06@gmail.com <rychkin06@gmail.com>
2022-02-05 14:25:36 +0300

    <Required>

--- Room.h
+++ Room.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <random>
 #include <iostream>
 #include "Player.h"
 #include <string>
@@ -6,24 +7,75 @@ class Room{
 private:
 	int PosX = 0;
 	int PosY = 0;
-	bool visible = true;
+	bool visible = false;
+	bool external = false;
+	int walls[4]{0, 0, 0, 0};
 	const std::string sigil = " ";
-public:
-	Room(int x, int y) {
-		PosX = x;
-		PosY = y;
+
+	void shuffle(int* arr)
+	{
+		srand(time(NULL));
+		for (int i = 4 - 1; i >= 1; i--)
+		{
+			int j = rand() % (i + 1);
+
+			int tmp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = tmp;
+		}
 	}
 
-	void SetVisible() {visible = false;}
-	void SetX(int x) {PosX = x;}
-	void SetY(int y) {PosY = y;}
-
-	void repr(int PlayerPosX, int PlayerPosY) {
-		if (PlayerPosX == PosX and PlayerPosY == PosY) {
-			std::cout << "{" + sigil + "}";
+public:
+	// constructors
+	Room() {}
+	Room(const Room& room) :
+		PosX(room.PosX), PosY(room.PosY), external(room.external), sigil(room.sigil) {}
+	
+	Room(int x, int y, int SIZE) {
+		PosX = x;
+		PosY = y;
+		if (PosY == 0 or PosY == SIZE - 1 or PosX == 0 or PosX == SIZE - 1) {
+			external = true;
 		}
 		else {
-			std::cout << '[' + sigil + ']';
+			for (int i = 0; i < std::rand() % 2 + 1; i++) {
+				walls[i] = 1;
+			}
+			shuffle(walls);
+		}
+	}
+
+	void SetVisible(bool vis) { visible = vis; }
+	void SetWall(int wallId, int New) { walls[wallId] = New; }
+	void SetX(int x) { PosX = x; }
+	void SetY(int y) { PosY = y; }
+	bool isExternal() { return external; }
+	bool isWall(int num) {
+		if (walls[num] != 0 or external) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	int GetX() { return PosX; }
+	int GetY() { return PosY; }
+
+	// room's representation
+	void repr(int PlayerPosX, int PlayerPosY) {
+		if (!external) {
+			if (PlayerPosX == PosX and PlayerPosY == PosY) {
+				std::cout << "\x1b[32m{\x1b[0m" + sigil + "\x1b[32m}\x1b[0m";
+			}
+			else if (visible) {
+				std::cout << '[' + sigil + ']';
+			}
+			else {
+				std::cout << "   ";
+			}
+		}
+		else {
+			std::cout << " # ";
 		}
 	}
 };
--- main.cpp
+++ main.cpp
@@ -5,30 +5,113 @@
 #include <iostream>
 using namespace std;
 Player player;
+vector <Room> maze;
 
-void showMaze(vector <Room> mtx, int SIZE) {
+// showing maze in console
+void showMaze(int SIZE) {
 	for (int y = 0; y < SIZE; y++) {
 		for (int x = 0; x < SIZE; x++) {
-			mtx[SIZE * y + x].repr(player.GetPosX(), player.GetPosY());
+			maze[SIZE * y + x].repr(player.GetPosX(), player.GetPosY());
 		}
-		std::cout << '\n';
+		cout << '\n';
 	}
-	std::cout << endl;
+	cout << endl;
 }
 
+// player's movement function 
+void playerMove(char wayChar, int SIZE) {
+	int chosenRoomId, way;
+	switch (wayChar) {
+	case 'u':
+		chosenRoomId = SIZE * (player.GetPosY() - 1) + player.GetPosX();
+		way = 0;
+		break;
+	case 'r':
+		chosenRoomId = SIZE * player.GetPosY() + player.GetPosX() + 1;
+		way = 2;
+		break;
+	case 'd':
+		chosenRoomId = SIZE * (player.GetPosY() + 1) + player.GetPosX();
+		way = 1;
+		break;
+	case 'l':
+		chosenRoomId = SIZE * player.GetPosY() + player.GetPosX() - 1;
+		way = 3;
+		break;
+	default:
+		cout << "\nYou are just sitting..." << endl;
+		return void();
+	}
+	Room yourRoom(player.GetPosX(), player.GetPosY(), SIZE);
 
-int main()
-{
-	int SIZE;
-	std::cout << "Size of the maze: ";
-	cin >> SIZE;
-	std::cout << endl;
-	vector <Room> maze;
-	for (int i = 0; i < SIZE; i++) {
-		for (int j = 0; j < SIZE; j++) {
-			maze.push_back(Room(j, i));
+	if (yourRoom.isWall(way) or maze[chosenRoomId].isWall((way + 2) % 4)) {
+		char ans;
+		cout << "\nThere is wall in front of you. Do you want to blow it? [Y/N]\n > ";
+		cin >> ans;
+		if (ans == 'y' and player.GetBombs() > 0) {
+			player.SetBombs(player.GetBombs() - 1);
+			if (maze[chosenRoomId].isExternal()) {
+				cout << "\nThis wall is too powerful! You can't blow it." << endl;
+			}
+			else {
+				cout << "\nYou've successfully blown up this wall!" << endl;
+				yourRoom.SetWall(way, 0);
+				maze[chosenRoomId].SetWall((way + 2) % 4, 0);
+				maze[chosenRoomId].SetVisible(true);
+				player.SetPosX(maze[chosenRoomId].GetX());
+				player.SetPosY(maze[chosenRoomId].GetY());
+			}
+		}
+		else if (ans == 'y' /* player.GetBombs() <= 0 */) {
+			cout << "\n\nYou haven't got enought bombs :(\nYou are just crying..." << endl;
+		}
+		else /*  if (ans != 'y')  */ {
+			cout << "\nYou are just sitting..." << endl;
 		}
 	}
-	showMaze(maze, SIZE);
+	else {
+		maze[chosenRoomId].SetVisible(true);
+		player.SetPosX(maze[chosenRoomId].GetX());
+		player.SetPosY(maze[chosenRoomId].GetY());
+	}
+}
+
+void statsShowWindow(Player player) {
+	string s = "----------";
+	cout << " #" << s << '#' << endl;
+	cout << " |" << " Bombs: " << player.GetBombs() << " |" << endl;
+	cout << " #" << s << '#' << endl;
+}
+
+int main() {
+	// size of the maze defining
+	int SIZE;
+	cout << "Size of the maze: ";
+	cin >> SIZE;
+	SIZE += 2;
+	cout << endl;
+
+	// maze forming
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			maze.push_back(Room(j, i, SIZE));
+		}
+	}
+
+	// start room chosing
+	player.SetPosX(rand() % (SIZE - 2) + 1);
+	player.SetPosY(rand() % (SIZE - 2) + 1);
+	maze[SIZE * player.GetPosY() + player.GetPosX()].SetVisible(true);
+
+	// game loop starting
+	char way;
+	while (true) {
+		showMaze(SIZE);
+		statsShowWindow(player);
+		cout << "\nYou are standing in the empty room. [U/R/D/L]\n > ";
+		cin >> way;
+		cout << endl;
+		playerMove(way, SIZE);
+	}
 	return 0;
 }
